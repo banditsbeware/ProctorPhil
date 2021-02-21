49344feba53760e717fb500ab05627ebf9d0c2e5
david rademacher <david.rademacher@mavs.uta.edu>
2021-02-21 12:53:19 -0600

    database redesign

--- database.py
+++ database.py
@@ -1,45 +1,59 @@
 import sqlite3
+
+VOCAB_CREATE = 'CREATE TABLE IF NOT EXISTS vocabulary (token TEXT UNIQUE, part TEXT)'
+VOCAB_INSERT = 'INSERT OR REPLACE INTO vocabulary VALUES (?,?)'
+VOCAB_SELECT = 'SELECT * FROM vocabulary WHERE part IS ?'
+
 class Database:
-  def __init__(self, _path):
-    self.path = _path
-    self.speech = ['noun', 'nounplural', 'nouncont', 'art1', 'art2', 'verb', 'verbs', 'verbing', 'adjective', 'adverb']
-    for part in self.speech:
-      self.execute(f'CREATE TABLE IF NOT EXISTS {part} (token text unique)')
+  def __init__(self, path):
+    self.conn = sqlite3.connect(path)
+    self.conn.execute(VOCAB_CREATE)
+    self.valid_parts = [
+      'NOUN-DISCRETE',          # egg, dog, hat
+      'NOUN-DISCRETE-PLURAL',   # eggs, dogs, hats
+      'NOUN-CONTINUOUS',        # water, science, milk
+      'VERB',                   # sing, die, exist
+      'VERB-TRANSITIVE',        # punch, look at, think about
+      'ARTICLE',                # the, my, his 
+      'ADJECTIVE',              # blue, difficult, sour
+      'ADVERB',                 # quickly, surprisingly
+      'MISC'                    # oooOOOooooOOOoOOOoOoo
+    ]
 
-  def reset(self, part=None):
-    if part is None:
-      for p in self.speech:
-        self.execute(f'DROP TABLE {p}')
-    else:
-      self.execute(f'DROP TABLE {part}')
-
+  def reset(self):
+    self.conn.execute(f'DROP TABLE vocabulary')
+    self.conn.commit()
     self.__init__(self.path)
   
-  def execute(self, operation):
-    conn = sqlite3.connect(self.path)
-    conn.execute(operation)
-    conn.commit()
-    conn.close()
-  
   def fetch(self, part):
-    cur = sqlite3.connect(self.path).cursor()
-    cur.execute(f'SELECT * FROM {part}')
-    return [token[0] for token in cur.fetchall()]
+    if part not in self.valid_parts: 
+      raise ValueError(f'database fetch failed\ninvalid part of speech: {part}')
 
-  def insert(self, part, word):
-    if part not in self.speech:
-      raise ValueError(f'Invalid part of speech: {part}')
-    if type(word) is list:
-      for w in word:
-        self.execute(f'INSERT OR REPLACE INTO {part} (token) VALUES (\'{w}\');')
+    cur = self.conn.cursor()
+    cur.execute(VOCAB_SELECT, (part,))
+    return cur.fetchall()
+
+  def insert(self, token, part):
+    if part not in self.valid_parts:
+      raise ValueError(f'database insert failed\ninvalid part of speech: {part}')
+
+    if type(token) is list:
+      # create a list of tuples, e.g.
+      # [('word 1', 'NOUN'), ('word 2', 'NOUN'), ('word 3', 'NOUN'),...]
+      values = [(i, part) for i in token]
     else:
-      self.execute(f'INSERT OR REPLACE INTO {part} (token) VALUES (\'{word}\');')
+      values = (token, part)
+
+    self.conn.execute(VOCAB_INSERT, values)
+    self.conn.commit()
 
   def count(self, part):
+    if part not in self.valid_parts:
+      raise ValueError(f'database count failed\ninvalid part of speech: {part}')
     return len(self.fetch(part))
 
   def summary(self):
-    for part in self.speech:
+    for part in self.valid_parts:
       print(f'{part}: {self.count(part)}')
 
 
